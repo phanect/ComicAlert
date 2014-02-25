6aafaa5096ef169445b68532d84bad486e8c1683
Jumpei Ogawa <phanective@gmail.com>
2014-02-25 20:52:24 +0900

    Refactoring

--- app/models/episode.js
+++ app/models/episode.js
@@ -1,48 +1,61 @@
-var Episode = function () {
+var Episode = function() {
 
-  this.defineProperties({
-    name: {type: 'string', required: true},
-    number: {type: 'number'},
-    subTitle: {type: 'string'},
-    updatedAt: {type: 'datetime'},
-    expireAt: {type: 'datetime'},
-    hasRead: {type: 'boolean'}
-  });
+	this.defineProperties({
+		name : {
+			type : 'string',
+			required : true
+		},
+		number : {
+			type : 'number'
+		},
+		subTitle : {
+			type : 'string'
+		},
+		updatedAt : {
+			type : 'datetime'
+		},
+		expireAt : {
+			type : 'datetime'
+		},
+		hasRead : {
+			type : 'boolean'
+		}
+	});
 
-  /*
-  this.property('login', 'string', {required: true});
-  this.property('password', 'string', {required: true});
-  this.property('lastName', 'string');
-  this.property('firstName', 'string');
+	/*
+	 this.property('login', 'string', {required: true});
+	 this.property('password', 'string', {required: true});
+	 this.property('lastName', 'string');
+	 this.property('firstName', 'string');
 
-  this.validatesPresent('login');
-  this.validatesFormat('login', /[a-z]+/, {message: 'Subdivisions!'});
-  this.validatesLength('login', {min: 3});
-  // Use with the name of the other parameter to compare with
-  this.validatesConfirmed('password', 'confirmPassword');
-  // Use with any function that returns a Boolean
-  this.validatesWithFunction('password', function (s) {
-      return s.length > 0;
-  });
+	 this.validatesPresent('login');
+	 this.validatesFormat('login', /[a-z]+/, {message: 'Subdivisions!'});
+	 this.validatesLength('login', {min: 3});
+	 // Use with the name of the other parameter to compare with
+	 this.validatesConfirmed('password', 'confirmPassword');
+	 // Use with any function that returns a Boolean
+	 this.validatesWithFunction('password', function (s) {
+	 return s.length > 0;
+	 });
 
-  // Can define methods for instances like this
-  this.someMethod = function () {
-    // Do some stuff
-  };
-  */
+	 // Can define methods for instances like this
+	 this.someMethod = function () {
+	 // Do some stuff
+	 };
+	 */
 
 };
 
 /*
-// Can also define them on the prototype
-Episode.prototype.someOtherMethod = function () {
-  // Do some other stuff
-};
-// Can also define static methods and properties
-Episode.someStaticMethod = function () {
-  // Do some other stuff
-};
-Episode.someStaticProperty = 'YYZ';
-*/
+ // Can also define them on the prototype
+ Episode.prototype.someOtherMethod = function () {
+ // Do some other stuff
+ };
+ // Can also define static methods and properties
+ Episode.someStaticMethod = function () {
+ // Do some other stuff
+ };
+ Episode.someStaticProperty = 'YYZ';
+ */
 
 Episode = geddy.model.register('Episode', Episode);
