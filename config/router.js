84e955cdc4e4bb5a9276a6d434850cd8aec23693
Jumpei Ogawa <phanective@gmail.com>
2014-03-12 19:20:51 +0900

    Refactoring

--- config/router.js
+++ config/router.js
@@ -18,26 +18,27 @@
 
 var router = new geddy.RegExpRouter();
 
-router.get('/').to('Main.index');
+router.get("/").to("Main.index");
 
 // Basic routes
-// router.match('/moving/pictures/:id', 'GET').to('Moving.pictures');
+// router.match("/moving/pictures/:id", "GET").to("Moving.pictures");
 //
-// router.match('/farewells/:farewelltype/kings/:kingid', 'GET').to('Farewells.kings');
+// router.match("/farewells/:farewelltype/kings/:kingid", "GET").to("Farewells.kings");
 //
 // Can also match specific HTTP methods only
-// router.get('/xandadu').to('Xanadu.specialHandler');
-// router.del('/xandadu/:id').to('Xanadu.killItWithFire');
+// router.get("/xandadu").to("Xanadu.specialHandler");
+// router.del("/xandadu/:id").to("Xanadu.killItWithFire");
 //
 // Resource-based routes
-// router.resource('hemispheres');
+// router.resource("hemispheres");
 //
 // Nested Resource-based routes
-// router.resource('hemispheres', function(){
-//   this.resource('countries');
-//   this.get('/print(.:format)').to('Hemispheres.print');
+// router.resource("hemispheres", function(){
+//   this.resource("countries");
+//   this.get("/print(.:format)").to("Hemispheres.print");
 // });
 
-router.resource('comics');
-router.resource('episodes');
+router.resource("comics");
+router.resource("episodes");
+
 exports.router = router;
