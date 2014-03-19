55a983a44a2c3fb79016fe97b0f8a08e7e18fe24
Jumpei Ogawa <phanective@gmail.com>
2014-03-19 20:58:05 +0900

    Refactor the structure of comicpage

--- crawler/comicpage.ts
+++ crawler/comicpage.ts
@@ -8,8 +8,14 @@ var moment = require("moment");
 require("../lib/commons");
 
 class ComicPage {
-	constructor(private url: string) {
+	comic: any;
+	episodes: Array<any> = new Array();
+	url: string;
+	
+	analyzeAndSave(url: string): void {
 		var html = "";
+		var self = this;
+		this.url = url;
 
 		http.get(url, function(response) {
 			response.on("data", function(chunk) {
@@ -27,37 +33,38 @@ class ComicPage {
 				var thumbnailUrl: string = $("img.cutImage").attr("src");
 				thumbnailUrl = "http://comic.mag-garden.co.jp/" + thumbnailUrl;
 
-				geddy.model.Comic.first({url : url}, function(err, comic) {
+				geddy.model.Comic.first({url : self.url}, function(err, data) {
 					if (err) {
 						throw err;
 					}
 
-					if (comic === undefined) { // New Comic
-						comic = geddy.model.Comic.create({
+					if (data === undefined) { // New Comic
+						self.comic = geddy.model.Comic.create({
 							title: title,
-							url: url,
+							url: self.url,
 							thumbnailUrl: thumbnailUrl
 						});
 						
-						if (comic.isValid()) {
-							comic.save(function (err, data) {
+						if (self.comic.isValid()) {
+							self.comic.save(function (err, data) {
 								if (err) {
 									throw err;
 								}
 							});
 						}
 					} else { // Existing Comic
-						if (title !== comic.title) {
-							comic.updateProperties({ title: title });
-							comic.save(function (err, data) {
+						self.comic = data;
+						if (title !== self.comic.title) {
+							self.comic.updateProperties({ title: title });
+							self.comic.save(function (err, data) {
 								if (err) {
 									throw err;
 								}
 							});
 						}
-						if (thumbnailUrl !== comic.thumbnailUrl) {
-							comic.updateProperties({ thumbnailUrl: thumbnailUrl });
-							comic.save(function (err, data) {
+						if (thumbnailUrl !== self.comic.thumbnailUrl) {
+							self.comic.updateProperties({ thumbnailUrl: thumbnailUrl });
+							self.comic.save(function (err, data) {
 								if (err) {
 									throw err;
 								}
@@ -70,15 +77,13 @@ class ComicPage {
 					//
 					var readBoxInner;
 					var text: string;
-					var episodeUrl: string;
-					var tmp;
-					var publishedAt: date;
-					var episodenum: number;
+					var tmp: any;
+					var episodeUrl: string, publishedAt: any, episodeNum: number;
 					$("div.read-box-inner").each(function(i, elem) {
 						readBoxInner = $(this);
 						text = readBoxInner.find("div.txt").find("h3").text();
 						text = text.toHalfWidth();
-						
+
 						episodeUrl = readBoxInner.find("div.txt").find("ul")
 										.find("li.readComic").find("a").attr("href");
 
@@ -95,57 +100,58 @@ class ComicPage {
 						// Extract Episode number
 						tmp = text.match(/\d{1,4}話/)[0];
 						if (tmp != null) {
-							episodenum = tmp[0];
+							episodeNum = tmp[0];
 						} else {
-							episodenum = null;
+							episodeNum = null;
+							return;
 						}
 						
-						if (episodenum !== null) {
-							geddy.model.Episode.first({comicId: comic.id, number: episodenum},
-									function(err, episode) {
-								
-								if (episode === undefined) { // New Episode
+						geddy.model.Episode.first({comicId: self.comic.id, number: episodeNum},
+								function(err, episode) {
+										
+							if (episode === undefined) { // New Episode
+								episode = geddy.model.Episode.create({
+									name : "第" + episodeNum + "話",
+									number : episodeNum,
+									url : episodeUrl,
+									publishedAt : publishedAt,
+									hasRead : false
+								});
 
-									episode = geddy.model.Episode.create({
-										name : "第" + episodenum + "話",
-										number : episodenum,
-										url : episodeUrl,
-										publishedAt : publishedAt,
-										hasRead : false
+								if (episode.isValid()) {
+									episode.save(function (err, data) {
+										if (err) {
+											throw err;
+										}
 									});
-
-									if (episode.isValid()) {
-										episode.save(function (err, data) {
-											if (err) {
-												throw err;
-											}
-										});
-									}
-									
-									// TODO Alert it to the users
-								} else { // Existing Episode
-									episode.updateProperties({
-										name : "第" + episodenum + "話",
-										number : episodenum,
-										url : episodeUrl,
-										publishedAt : publishedAt,
-									});
-									if (episode.isValid()) {
-										episode.save(function (err, data) {
-											if (err) {
-												throw err;
-											}
-										});
-									}
 								}
-							});
-						}
+								
+								// TODO Alert it to the users
+							} else { // Existing Episode
+								episode.updateProperties({
+									name : "第" + episodeNum + "話",
+									number : episodeNum,
+									url : episodeUrl,
+									publishedAt : publishedAt
+								});
+								if (episode.isValid()) {
+									episode.save(function (err, data) {
+										if (err) {
+											throw err;
+										}
+									});
+									
+									
+								}
+							}
+							self.episodes.push(episode);
+						});
 					});
 				});
 			});
 		});
-
 	}
+
 }
 
 export = ComicPage;
