97fae8aba850b4df6013da0cc7e21d2ba67005f2
Jumpei Ogawa <phanective@gmail.com>
2014-04-09 21:51:36 +0900

    Add test for

--- test/models/user.js
+++ test/models/user.js
@@ -19,6 +19,33 @@ var tests = {
 			should.not.exist(err);
 			next();
 		});
+	},
+	"Test user.connectComics()" : function() {
+		var user = testdata.unSavedUser();
+		var comic = testdata.unSavedComic();
+		
+		user.save(function(err, data) {
+			if (err) {
+				should.not.exist(err);
+			}
+			comic.save(function(err, data) {
+				if (err) {
+					should.not.exist(err);
+				}
+				
+				user.connectComics(comic);
+
+				user.getComics(function(err, comics) {
+					comic.id.should.equal(comics[0].id);
+				});
+
+				comic.getUsers(function(err, users) {
+					user.id.should.equal(users[0].id);
+				});
+			});
+		});
+		
+
 	}
 };
 
