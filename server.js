a304a636f0eabb83651ec88299ba7168881873eb
Jumpei Ogawa <phanective@gmail.com>
2014-07-08 21:35:58 +0900

    Change environment from development to production

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ var geddy = require("geddy");
 geddy.start({
 	port: process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || "3000",
 	hostname: process.env.OPENSHIFT_NODEJS_IP || "localhost",
-	environment: "development"
+	environment: "production"
 });
