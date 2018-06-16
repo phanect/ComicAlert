53622077553cc92712dc2c634c03c7cc4e3771c6
Jumpei Ogawa <phanective@gmail.com>
2018-06-17 00:45:47 +0900

    refactor(crawler): reimplement MagGarden.analyzeMagazinePage()

--- crawler/MagGarden.ts
+++ crawler/MagGarden.ts
@@ -4,26 +4,33 @@ import { JSDOM } from "jsdom";
 
 import { Comic } from "./Comic";
 import { Episode } from "./Episode";
+import { Magazine } from "./Magazine";
 
 export class MagGarden {
+  private magazines: Magazine[] = [];
+
+  get Magazines() {
+    return this.magazines;
+  }
+
   constructor(private url: string) {
   }
 
-  analyzeMagazinePage($: any, cb : any) {
-    const self = this;
+  async analyzeMagazinePage(opt: { url: string, name: string }): Promise<any> {
+    const document = new JSDOM("", {
+            url: opt.url,
+          }).window.document,
+          magazine = new Magazine(opt.name),
+          comics: Comic[] = [];
 
-    $("div#comicList02 > ul > li").each((i, elem) => {
-      const comicUrl = elem.find("a").attr("href");
+    for (const comicBox of document.querySelectorAll("article.cbox")) {
+      const comicURL = comicBox.querySelector(".inner > a.cbox-main").getAttribute("href");
 
-      if (comicUrl != self.url && comicUrl.includes(self.url)) {
-        self.comicUrls.push(comicUrl);
-      }
+      comics.push(await this.analyzeComicPage(comicURL));
+    }
 
-      // the last element
-      if (i + 1 === $("div#comicList02 > ul > li").length) {
-        cb();
-      }
-    });
+    magazine.Comics = comics;
+    this.magazines.push(magazine);
   }
 
   async analyzeComicPage(url: string): Promise<Comic> {
