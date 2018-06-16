141869dd867adb306e22dadbbcbe79cff6b30a9a
Jumpei Ogawa <phanective@gmail.com>
2018-06-17 00:01:22 +0900

    refactor(crawler): merge MagGardenMagazineIndex & MagGardenComicPage classes to MagGarden
    Method names are changed as follows:
    - MagGardenMagazineIndex.analyzeComics() -> MagGarden.analyzeMagazinePage()
    - MagGardenComicPage.analyze() -> MagGarden.analyzeComicPage()

--- modules/crawler/MagGarden.ts
+++ modules/crawler/MagGarden.ts
@@ -1,15 +1,30 @@
 import { fixchar } from "fixchar";
 import * as moment from "moment";
 
-import { Comic } from "./interfaces/Comic";
-
-export class MagGardenComic implements Comic {
-  title: string;
-
+export class MagGarden {
   constructor(private url: string) {
   }
 
-  async analyze() {
+  async analyzeMagazinePage($: any): Promise<void> {
+    const self = this;
+
+    return new Promise((resolve, reject) => {
+      $("div#comicList02 > ul > li").each((i, elem) => {
+        const comicUrl = elem.find("a").attr("href");
+
+        if (comicUrl != self.url && comicUrl.includes(self.url)) {
+          self.comicUrls.push(comicUrl);
+        }
+
+        // the last element
+        if (i + 1 === $("div#comicList02 > ul > li").length) {
+          resolve();
+        }
+      });
+    });
+  }
+
+  async analyzeComicPage() {
     //
     // Scraping title
     //
