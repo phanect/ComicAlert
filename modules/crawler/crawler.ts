fedcd02426ad80f74ca938df514a1a2bccd86ea2
Jumpei Ogawa <phanect@users.noreply.github.com>
2019-02-13 23:38:43 +0900

    feat[crawler]: save crawled comics data as JSON and deploy it on GitHub Pages
    At the early stage, I save comic data as JSON and save it on static
    hosting.
    I may consider to migrate it to any kind of  relational or NoSQL
    databas.

--- modules/crawler/crawler.ts
+++ modules/crawler/crawler.ts
@@ -1,3 +1,5 @@
+import { writeFile, ensureDir } from "fs-extra";
+import { publish } from "gh-pages";
 import { join } from "path";
 
 import { MagGarden } from "./MagGarden";
@@ -8,4 +10,21 @@ import { MagGarden } from "./MagGarden";
 
   await magGarden.crawl();
 
+  await ensureDir(jsonDir);
+  await Promise.all([
+    writeFile(join(jsonDir, "CNAME"), "api.comicstand.phanective.org"),
+    writeFile(join(jsonDir, "comics.json"), JSON.stringify(magGarden.Magazines))
+  ]);
+
+  return new Promise((resolve, reject) => {
+    publish("dist", {
+      repo: "git@github.com:phanect/static-comicstand-data.git",
+    }, (err: Error) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
 })();
