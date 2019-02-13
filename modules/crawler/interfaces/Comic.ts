3fc610c2a28585f696a8f8670073e2a0cbb9a7e1
Jumpei Ogawa <phanect@users.noreply.github.com>
2019-02-13 23:35:56 +0900

    refactor: transform ComicPage & MagazineIndex as interfaces (file modification)

--- modules/crawler/interfaces/Comic.ts
+++ modules/crawler/interfaces/Comic.ts
@@ -1,14 +1,9 @@
-import * as cheerio from "cheerio";
-import fetch from "node-fetch";
+import { Episode } from "./Episode"
 
-export class ComicPage {
-  comic : any;
-  title : string;
-  thumbnailUrl : string;
-  episodes : Array<any> = new Array();
-  concluded : boolean = false;
-
-  constructor(public url : string) {
-
-  }
+export interface Comic {
+  url: string;
+  title: string;
+  thumbnailURL: string;
+  episodes: Episode[];
+  concluded: boolean;
 }
--- modules/crawler/interfaces/Magazine.ts
+++ modules/crawler/interfaces/Magazine.ts
@@ -1,32 +1,7 @@
-import * as cheerio from "cheerio";
-import fetch from "node-fetch";
+import { Comic } from "./Comic";
 
-export class MagazineIndex {
-  comicUrls : Array<string> = new Array();
-
-  constructor(public url: string, private name: string) {
-    console.log("Analyzing Magazine:", this.name, "<", this.url, ">");
-  }
-
-  async analyzeComics($ : any) : Promise<void> {
-    throw new Error("This method must be overrided. Aren't you using MagazineIndex class directly?");
-  }
-
-  private async analyze(): Promise<void> {
-    const self = this;
-
-    try {
-      const res = await fetch(self.url),
-            html = await res.text(),
-            $ = cheerio.load(html);
-
-      if (res.status !== 200) {
-        throw new Error("Return status code " + res.status);
-      }
-
-      return self.analyzeComics($);
-    } catch (err) {
-      console.error(err);
-    }
-  }
+export interface Magazine {
+  id: string;
+  name: string;
+  comics: Comic[];
 }
