289103e46b57fa31f06cfb4771cc2eabd1f5284f
Jumpei Ogawa <phanect@users.noreply.github.com>
2019-04-08 22:26:53 +0900

    chore: add temporary ESLint config for React

--- modules/frontend/.eslintrc.js
+++ modules/frontend/.eslintrc.js
@@ -1,8 +1,34 @@
 "use strict";
 
 module.exports = {
+  extends: [
+    "plugin:@phanect/js",
+    "plugin:react/recommended"
+  ],
   env: {
     browser: true,
     node: false,
   },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    }
+  },
+  settings: {
+    react: {
+      createClass: "createReactClass",
+      pragma: "React",
+      version: "16.x",
+    },
+    "propWrapperFunctions": [
+        // The names of any function used to wrap propTypes, e.g. `forbidExtraProps`. If this isn't set, any propTypes wrapped in a function will be skipped.
+        "forbidExtraProps",
+        {"property": "freeze", "object": "Object"},
+        {"property": "myFavoriteWrapper"}
+    ],
+    "linkComponents": [
+      "Hyperlink",
+      { "name": "Link", "linkAttribute": "href" }
+    ]
+  }
 };
