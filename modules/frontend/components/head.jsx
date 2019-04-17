abdb3f6c4739b351006f05913197493d6fc23b05
Jumpei Ogawa <phanect@users.noreply.github.com>
2019-04-18 00:01:25 +0900

    feat: update <head> contents

--- modules/frontend/components/head.jsx
+++ modules/frontend/components/head.jsx
@@ -4,12 +4,15 @@ import React from "react";
 import NextHead from "next/head";
 import PropTypes from "prop-types";
 
+const origin = "https://comicstand.phanective.org",
+      defaultDescription = "Comic Standは、無料のWebコミックを集めた漫画ポータルサイトです。";
+
 const Head = props => (
   <NextHead>
-    <title>Comic Alert</title>
-    {/* <meta name="description" content="" /> */}
+    <title>{props.title + " ― " || ""}Comic Stand</title>
+    <meta name="description" content={props.description || defaultDescription} />
     <meta name="author" content="phanect" />
-    <meta name="viewport" content="width=device-width" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
   </NextHead>
 );
 
