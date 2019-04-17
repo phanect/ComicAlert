018fdde7d014cbf3c70ae83d5f474fb2a280ca07
Jumpei Ogawa <phanect@users.noreply.github.com>
2019-04-17 23:53:25 +0900

    feat: add tabbar using @material/react-tab-bar

--- modules/frontend/layout/app.jsx
+++ modules/frontend/layout/app.jsx
@@ -1,9 +1,25 @@
 "use strict";
 
+import Tab from "@material/react-tab";
+import TabBar from "@material/react-tab-bar";
 import React from "react";
 import PropTypes from "prop-types";
 
+import "@material/react-tab-bar/dist/tab-bar.css";
+import "@material/react-tab-scroller/dist/tab-scroller.css";
+import "@material/react-tab/dist/tab.css";
+import "@material/react-tab-indicator/dist/tab-indicator.css";
+
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      activeIndex: 0,
+    };
+    this.handleActiveIndexUpdate = (activeIndex) => this.setState({ activeIndex });
+  }
+
   render() {
     return (
       <div>
@@ -12,6 +28,20 @@ class App extends React.Component {
             {this.props.children}
           </div>
         </div>
+        <TabBar
+          activeIndex={this.state.activeIndex}
+          handleActiveIndexUpdate={this.handleActiveIndexUpdate}
+        >
+          <Tab>
+            <span className="mdc-tab__text-label">お気に入り</span>
+          </Tab>
+          <Tab>
+            <span className="mdc-tab__text-label">検索</span>
+          </Tab>
+          <Tab>
+            <span className="mdc-tab__text-label">設定</span>
+          </Tab>
+        </TabBar>
       </div>
     );
   }
