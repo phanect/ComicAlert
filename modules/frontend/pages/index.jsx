1762c14841376166d1291fdbf4dc6b9c8ee3ae80
Jumpei Ogawa <phanective@gmail.com>
2019-09-05 22:34:12 +0900

    feat: load updated comic data from the web API

--- modules/frontend/pages/index.jsx
+++ modules/frontend/pages/index.jsx
@@ -1,5 +1,6 @@
 "use strict";
 
+import fetch from "isomorphic-unfetch";
 import React from "react";
 import App from "../layout/app";
 import ComicList from "../components/comic-list";
@@ -11,47 +12,23 @@ export default class Index extends React.Component {
   constructor() {
     super();
 
-    this.state = {
-      favs: [
-        {
-          url: "https://comic.mag-garden.co.jp/psychoss2/",
-          title: "PSYCHO-PASS サイコパス Sinners of the System Case.2 First Guardian",
-          thumbnailURL:"https://comic.mag-garden.co.jp/wordpress/wp-content/uploads/psychoss2-thumb.jpg",
-          concluded:false,
-          episodes:[
-            {
-              title:"第1話",
-              pcURL:"/assets/files/psychoss2190215/HTML5/pc.html",
-              mobileURL:"/assets/files/psychoss2190215/HTML5/sd.html",
-              publishedAt:"2019-02-16T09:52:15.169Z",
-              expiresAt:null,
-            },
-          ],
-        },
-        {
-          url:"https://comic.mag-garden.co.jp/graveyard/",
-          title:"グレイヴヤードの捩じくれた家",
-          thumbnailURL:"https://comic.mag-garden.co.jp/wordpress/wp-content/uploads/graveyard-thumb.jpg",
-          concluded:false,
-          episodes:[
-            {
-              title:"第2話",
-              pcURL:"/assets/files/graveyard190210/HTML5/pc.html",
-              mobileURL:"/assets/files/graveyard190210/HTML5/sd.html",
-              publishedAt:"2019-02-16T09:52:17.333Z",
-              expiresAt:null,
-            },
-            {
-              title:"第1話",
-              pcURL:"/assets/files/graveyard190110/HTML5/pc.html",
-              mobileURL:"/assets/files/graveyard190110/HTML5/sd.html",
-              publishedAt:"2019-02-16T09:52:17.333Z",
-              expiresAt:null,
-            },
-          ],
-        },
-      ],
+    const self = this;
+
+    self.state = {
+      favs: [],
+      newArrivals: [],
+      updates: [],
     };
+
+    (async () => {
+      try {
+        self.setState({
+          updates: await (await fetch("https://api-comicstand.phanective.org/comics.json")).json(),
+        });
+      } catch(err) {
+        console.log(err); // TODO
+      }
+    })();
   }
 
   render() {
@@ -60,8 +37,8 @@ export default class Index extends React.Component {
         <Head path="/" />
         <main>
           <ComicList title="お気に入り" comics={this.state.favs} />
-          <ComicList title="新連載・読切" comics={this.state.favs} />
-          <ComicList title="更新" comics={this.state.favs} />
+          <ComicList title="新連載・読切" comics={this.state.newArrivals} />
+          <ComicList title="更新" comics={this.state.updates} />
         </main>
       </App>
     );
