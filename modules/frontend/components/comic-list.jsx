c7ee4e0d05e1482f8909e5c6cbb80323f553b513
Jumpei Ogawa <phanective@gmail.com>
2019-09-05 22:53:40 +0900

    feat: do not show the title when no comics given

--- modules/frontend/components/comic-list.jsx
+++ modules/frontend/components/comic-list.jsx
@@ -5,28 +5,34 @@ import PropTypes from "prop-types";
 
 import "./comic-list.scss";
 
-const ComicList = props => (
-  <div>
-    <h2 className="list-title">{props.title}</h2>
-    <div>
-      {props.comics.map((comic, i) => (
-        <figure className="comic" key={i}>
-          <img
-            src={comic.thumbnailURL}
-            alt="{comic.title}"
-            className="thumbnail"
-            decoding="async"
-          />
-          <figcaption>{comic.title}</figcaption>
-          <div className="latest-episode">
-            {comic.episodes[0].title}<br />
-            {new Date(comic.episodes[0].publishedAt).toLocaleDateString("ja-JP")} 更新
-          </div>
-        </figure>
-      ))}
-    </div>
-  </div>
-);
+const ComicList = props => {
+  if (0 < props.comics.length) {
+    return (
+      <div>
+        <h2 className="list-title">{props.title}</h2>
+        <div>
+          {props.comics.map((comic, i) => (
+            <figure className="comic" key={i}>
+              <img
+                src={comic.thumbnailURL}
+                alt="{comic.title}"
+                className="thumbnail"
+                decoding="async"
+              />
+              <figcaption>{comic.title}</figcaption>
+              <div className="latest-episode">
+                {comic.episodes[0].title}<br />
+                {new Date(comic.episodes[0].publishedAt).toLocaleDateString("ja-JP")} 更新
+              </div>
+            </figure>
+          ))}
+        </div>
+      </div>
+    );
+  } else {
+    return "";
+  }
+};
 
 ComicList.propTypes = {
   title: PropTypes.string,
