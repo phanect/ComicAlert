19fcc41fb32a762f3f95a89389fbc33415d98746
Jumpei Ogawa <phanective@gmail.com>
2021-06-28 03:24:58 +0900

    chore: remove unnecessary <meta name="author">

--- modules/frontend/components/head.jsx
+++ modules/frontend/components/head.jsx
@@ -11,7 +11,6 @@ const Head = props => (
   <NextHead>
     <title>{props.title + " ― " || ""}Comic Stand</title>
     <meta name="description" content={props.description || defaultDescription} />
-    <meta name="author" content="phanect" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
   </NextHead>
 );
