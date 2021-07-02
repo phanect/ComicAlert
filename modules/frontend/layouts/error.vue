3a6bc781c625fae18afb18f55891c9b9bc8c3998
Jumpei Ogawa <phanective@gmail.com>
2021-07-03 00:43:31 +0900

    refactor: rewrite error page from ejs to Vue

--- modules/frontend/layouts/error.vue
+++ modules/frontend/layouts/error.vue
@@ -1,7 +1,14 @@
-<h2>Error: <%= statusCode %> <%= statusText %></h2>
-<% if (message) { %>
-<div>
-  <%= message; %>
-</div>
-<% } %>
+<template>
+  <div>
+    <h2>Error: {{ error.statusCode }}</h2>
+    <div>
+      {{ error.message }}
+    </div>
+  </div>
+</template>
 
+<script>
+export default {
+  props: [ "error" ],
+}
+</script>
